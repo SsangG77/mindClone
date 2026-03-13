72d56a8e78a1cdabe0ae3192cbcf1cdb1501aa29
sangjin <sangjincha719@gmail.com>
2026-03-13 13:51:40 +0900

    Initial Commit

---
+++ mindClone/mindCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  mindCloneApp.swift
+//  mindClone
+//
+//  Created by 김무경 on 3/13/26.
+//
+
+import SwiftUI
+
+@main
+struct mindCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
